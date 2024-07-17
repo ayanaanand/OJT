412eb7a0733d86e269163ffc99f0ecd4b80234ac
ayanaanand <122530612+ayanaanand@users.noreply.github.com>
2024-07-17 09:27:15 +0530

    Delete Daily task/03-06-2024 directory

--- Daily task/03-06-2024/q1.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-array = np.array([1, 0, 2, 0, 3, 4])
-count_nonzero = np.count_nonzero(array)
-print(count_nonzero)
--- Daily task/03-06-2024/q6.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-matrix = np.array([[1, 2], [3, 4]])
-inverse_matrix = np.linalg.inv(matrix)
-print("Inverse matrix:\n", inverse_matrix)
